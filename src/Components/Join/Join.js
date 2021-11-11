9657527d4f80c877549755dd1814ab9b5be06ed3
Saumya <saumyasharma@coresight.com>
2021-11-11 17:41:43 -0500

    initial chat box created

---
+++ src/Components/Join/Join.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Join.css';
+
+const Join = () => {
+	const [ name, setName ] = useState('');
+	const [ room, setRoom ] = useState('');
+
+	return (
+		<div className="joinOuterContainer">
+			<div className="joinInnerContainer">
+				<h1 className="heading">Join</h1>
+				<div><input placeholder="Name" type="text" onChange={(e) => setName(e.target.value)} className="joinInput" /></div>
+				<div><input placeholder="Room" type="text" onChange={(e) => setRoom(e.target.value)} className="joinInput mt-20" /></div>
+				<Link onClick={e => (!name || !room) ? e.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
+					<button className="button mt-20" type="submit">Sign in</button>
+				</Link>
+			</div>
+		</div>
+	)
+}
+
+export default Join;
---
+++ src/Components/Message/Message.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Message.css';
+
+const Message = ({msg: {user, text}, name}) => {
+	let isSentByCurrentUser = false;
+	const trimmedName = name.trim().toLowerCase();
+	if( user === trimmedName ) isSentByCurrentUser = true;
+	return(
+		isSentByCurrentUser
+		?
+		(
+			<div className="msgContainer justifyEnd">
+				<p className="sentText pr-10">{trimmedName}</p>
+				<div className="msgBox bgBlue">
+					<p className="msgText colorLight">{text}</p>
+				</div>
+			</div>
+		)
+		: (
+			<div className="msgContainer justifyStart">
+				<div className="msgBox bgLight">
+					<p className="msgText colorDark">{text}</p>
+				</div>
+				<p className="sentText pl-10">{user}</p>
+			</div>
+		)
+	)
+}
+
+export default Message;
