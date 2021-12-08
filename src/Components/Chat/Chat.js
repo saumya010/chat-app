ea16fa1e1fd5fbfd138c83196bce53df18faadad
Saumya <saumyasharma@coresight.com>
2021-12-08 14:13:19 -0500

    updated endpoint

--- src/Components/Chat/Chat.js
+++ src/Components/Chat/Chat.js
@@ -15,7 +15,7 @@ const Chat = () => {
 	const [users, setUsers] = useState('');
 	const [message, setMessage] = useState('');
 	const [messages, setMessages] = useState([]);
-	const ENDPOINT = 'localhost:5000';
+	const ENDPOINT = 'https://chat-applicatn-react.herokuapp.com/';
 
 	useEffect(() => {
 		const {name, room} = queryString.parse(window.location.search)
